f52996eeb0f7956f7f80bef7c1f1077cec041954
Bhavya Bharti <bhavy@ramank-home.redmond.corp.microsoft.com>
2019-12-22 00:35:08 -0800

    png and MocData added

---
+++ src/webparts/studentPage/MocStudentData.ts
@@ -0,0 +1,26 @@
+export q MocStudentData {
+    name: 'Vihaan Roshan';
+    // studentId: string;
+    // class: string;
+    // room: string;
+    // familyId: string;
+    // attendanceAndHomeworkstatus: {
+    //   lastClass: {
+    //     date: string;
+    //     attendance: string;
+    //     homeworkStatus: {
+    //       culture: string;
+    //       language: string;
+    //       spoken: string;
+    //     }
+    //   };
+    //   commulative: {
+    //     attendance: string;
+    //     homeworkGrade: {
+    //       culture: string;
+    //       language: string;
+    //       spoken: string;
+    //     }
+    //   }
+    // };
+}
