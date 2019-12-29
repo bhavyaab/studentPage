cbb6486fe34508da6199ebe7871e107fdbd4d83a
Bhavya Bharti <bhavy@ramank-home.redmond.corp.microsoft.com>
2019-12-29 02:22:39 -0800

    set to default location

---
+++ src/webparts/studentPage/components/StudentPage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import styles from './StudentPage.module.scss';
+import { IStudentPageProps } from './IStudentPageProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class StudentPage extends React.Component<IStudentPageProps, {}> {
+  public render(): React.ReactElement<IStudentPageProps> {
+    return (
+      <div className={ styles.studentPage }>
+        <p className={ styles.name }>{escape(this.props.name)}</p>
+      </div>
+    );
+  }
+}
---
+++ src/webparts/studentPage/loc/mystrings.d.ts
@@ -0,0 +1,11 @@
+declare interface IStudentPageWebPartStrings {
+  PropertyPaneName: any;
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  NameFieldLabel: string;
+}
+
+declare module 'StudentPageWebPartStrings' {
+  const strings: IStudentPageWebPartStrings;
+  export = strings;
+}
